918e5de43b5715aefd26f3371503dbe15a7fac73
Najih Driss <najih.driss73@gmail.com>
2021-12-04 16:02:52 +0100

    Create index.tsx

---
+++ pages/deal/index.tsx
@@ -0,0 +1,9 @@
+import { PageComponent } from "@utils/common-types";
+
+interface Props {}
+
+const DealsPage: PageComponent<Props> = () => {
+  return <div>Deals Page</div>;
+};
+
+export default DealsPage;
